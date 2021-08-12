dd75fc4e33c39333bfbe5c321f26d439ed03ebac
naome-turbo <73062047+naome-turbo@users.noreply.github.com>
2021-08-12 22:49:01 +0300

    Add files via upload
    Изменены наименования profile

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 const popUp = document.querySelector('.popup');
-const info = document.querySelector('.info');
-const openEditBtn = document.getElementById('open_edit_btn');
+const info = document.querySelector('.profile__info');
+const openEditBtn = document.getElementById('profile_edit_btn');
 const closeEditBtn = document.querySelector('.popup__button-exit');
-const infoName = info.querySelector('.info__name');
-const infoDescription = info.querySelector('.info__description');
+const infoName = info.querySelector('.profile__name');
+const infoDescription = info.querySelector('.profile__description');
 const editName = document.querySelector('#popup__input_name');
 const editDescription = document.querySelector('#popup__input_description');
 let formElement = document.querySelector('.popup__content');
@@ -12,12 +12,10 @@ function openEdit() {
   popUp.classList.add('popup_visibility');
   editName.value = infoName.textContent;
   editDescription.value = infoDescription.textContent;
-  // popUp.style.visibility = "visible";
 }
 
 function closeEdit() {
   popUp.classList.remove('popup_visibility');
-  // popUp.style.visibility = "hidden";
 }
 
 function editInfo() {
