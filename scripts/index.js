36f137c183e7fb4770c4c9b79ecda592d80d3b34
naome-turbo <73062047+naome-turbo@users.noreply.github.com>
2021-09-27 15:10:54 +0300

    Update index.js
    Доработана возможность вариативной работы с формой1

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,35 @@
 const popUp = document.querySelector('.popup');
 const info = document.querySelector('.profile__info');
 const openEditBtn = document.getElementById('profile_edit_btn');
+const openAddBtn = document.getElementById('profile__add');
 const closeEditBtn = document.querySelector('.popup__button-exit');
-const infoName = info.querySelector('.profile__name');
-const infoDescription = info.querySelector('.profile__description');
-const editName = document.querySelector('#popup__input_name');
-const editDescription = document.querySelector('#popup__input_description');
+
+const formName = document.querySelector('.popup__title');
+const profName = info.querySelector('.profile__name');
+const cardName = info.querySelector('.grid-menu__name');
+const profDescription = info.querySelector('.profile__description');
+const cardDescription = info.querySelector('.cards-grid__item');
+
+const crdsTemplate = document.querySelector('#cards-grid__list-item').content;
+const crds = document.querySelector('.cards-grid__list');
+
+let editName = document.querySelector('#popup__input_name');
+let editDescription = document.querySelector('#popup__input_description');
 let formElement = document.querySelector('.popup__content');
+let btn;
 
 function openEdit() {
+  if (btn === 'add') {
+    formName.textContent = "Новое место";
+    editName.value = "Название";
+    editDescription.value = "Ссылка на картинку";
+  } else {
+    formName.textContent = "Редактировать профиль";
+    editName.value = profName.textContent;
+    editDescription.value = profDescription.textContent;
+  }
+
   popUp.classList.add('popup_visibility');
-  editName.value = infoName.textContent;
-  editDescription.value = infoDescription.textContent;
 }
 
 function closeEdit() {
@@ -19,8 +37,15 @@ function closeEdit() {
 }
 
 function editInfo() {
-  infoName.textContent = editName.value;
-  infoDescription.textContent = editDescription.value;
+  if (btn === 'add') {
+    const crdImg = cardsTemplate.querySelector('.cards-grid__list-item').cloneNode(true);
+    crdImg.querySelector('.cards-grid__item').src = editDescription.value;
+    crdImg.querySelector('.grid-menu__name').textContent = editName.value;
+    crds.prepend(crdImg);
+  } else {
+    profName.textContent = editName.value;
+    profDescription.textContent = editDescription.value;
+  }
 }
 
 function saveEdit(e) {
@@ -29,7 +54,8 @@ function saveEdit(e) {
   closeEdit();
 }
 
-openEditBtn.addEventListener('click', openEdit);
+openEditBtn.addEventListener('click', function(){ openEdit(btn = 'ed'); });
+openAddBtn.addEventListener('click', function(){ openEdit(btn = 'add'); });
 
 closeEditBtn.addEventListener('click', closeEdit);
 
